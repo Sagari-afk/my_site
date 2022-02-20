8549f014d2ae2fb914aa329f9630a5ceb429db89
Sagari-afk <lergo2004@gmail.com>
2022-02-20 13:41:32 +0200

    Реализована RESTархитектура для приложения users

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,3 @@
-"""from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
-"""
-
 from rest_framework import routers
 
 from .views import UserViewSet
